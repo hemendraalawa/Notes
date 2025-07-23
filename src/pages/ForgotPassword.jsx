941b06446d225e020ffd2aaf516c8b1edadf28bf
Hemendra Alawa <hemendraalawa2017@gmail.com>
2025-07-23 19:20:08 +0530

    Update ForgotPassword.jsx  backend url updated

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -11,7 +11,7 @@ const ForgotPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/forgot-password", {
+      const res = await axios.post("https://notes-backend-e62d.onrender.com/api/auth/forgot-password", {
         email,
       });
 
